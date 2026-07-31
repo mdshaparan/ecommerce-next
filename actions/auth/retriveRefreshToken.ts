389ad01f38973471b60b9d50941116423094941c
mdshaparan <mdshaparanahammed@gmail.com>
2026-08-01 01:05:51 +0600

    Add environment configuration, API client setup, and token management functions

---
+++ actions/auth/retriveRefreshToken.ts
@@ -0,0 +1,8 @@
+'user server';
+
+import {cookies} from 'next/headers';
+
+export default async function retrieveRefreshToken() {
+  const cookieStore = await cookies();
+  return cookieStore.get('refreshToken')?.value;
+}
